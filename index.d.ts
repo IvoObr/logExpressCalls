e0125f92966a779d731e9d9cd27d13f5298528b8
IvoObr <ivo_obr@hotmail.com>
2023-08-14 12:41:14 +0300

    logIngnorePath

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,12 @@
 declare class Log {
     private static styles;
+    private static ignorePath;
     private static colors;
     private static getTime;
     private static getColoredStatus;
+    static logIgnorePath(ignorePath: string): void;
     static logCalls(request: any, response: any, next: any): void;
 }
 export declare const logExpress: typeof Log.logCalls;
+export declare const logIgnorePath: typeof Log.logIgnorePath;
 export {};
