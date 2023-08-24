89d1ff2f035232824172a887faf054b8e86bbcd0
IvoObr <ivo_obr@hotmail.com>
2023-08-24 13:40:26 +0300

    colorless mode

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
-declare class Log {
+export declare class Log {
     private static styles;
-    private static ignorePath;
+    private static ignore;
+    private static isColorless;
     private static colors;
     private static getTime;
     private static getColoredStatus;
-    static logIgnorePath(ignorePath: string): void;
+    static ignorePath(ignorePath: string): void;
+    static useColorless(): void;
     static logCalls(request: any, response: any, next: any): void;
 }
 export declare const logExpress: typeof Log.logCalls;
-export declare const logIgnorePath: typeof Log.logIgnorePath;
-export {};
--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-class Log {
+export class Log {
   private static styles = {
     reset: "\x1b[0m",
     bold: "\u001b[1m",
   };
 
-  private static ignorePath: string[] = [];
+  private static ignore: string[] = [];
+  private static isColorless: boolean = false;
 
   private static colors = {
     red: "\x1b[31m",
@@ -15,25 +16,25 @@ class Log {
   };
 
   private static getTime(): string {
-    const time: string = new Date()
-      .toISOString()
-      .replace("T", " ")
-      .substring(0, 19);
+    const time: string = new Date().toISOString().replace("T", " ").substring(0, 19);
     return `[${time}]`;
   }
 
   private static getColoredStatus(status: number): string {
-    if (status >= 500) return `${Log.colors.red}${status}${Log.styles.reset}`;
-    if (status >= 400)
-      return `${Log.colors.yellow}${status}${Log.styles.reset}`;
-    if (status >= 300) return `${Log.colors.cyan}${status}${Log.styles.reset}`;
-    if (status >= 200) return `${Log.colors.green}${status}${Log.styles.reset}`;
+    if (status >= 500) return Log.isColorless ? `${status}` : `${Log.colors.red}${status}${Log.styles.reset}`;
+    if (status >= 400) return Log.isColorless ? `${status}` : `${Log.colors.yellow}${status}${Log.styles.reset}`;
+    if (status >= 300) return Log.isColorless ? `${status}` : `${Log.colors.cyan}${status}${Log.styles.reset}`;
+    if (status >= 200) return Log.isColorless ? `${status}` : `${Log.colors.green}${status}${Log.styles.reset}`;
 
     return `${status}`;
   }
 
-  public static logIgnorePath(ignorePath: string): void {
-    Log.ignorePath.push(ignorePath)
+  public static ignorePath(ignorePath: string): void {
+    Log.ignore.push(ignorePath);
+  }
+
+  public static useColorless(): void {
+    Log.isColorless = true;
   }
 
   public static logCalls(request: any, response: any, next: any): void {
@@ -46,9 +47,15 @@ class Log {
 
       const method: string = response?.req?.method as string;
       const timestamp: string = Log.getTime();
-      const message: string = `${Log.styles.bold}${timestamp} ${method}: ${url} ${status} ${endTime}ms${Log.styles.reset}`;
+      let message: string;
 
-      if (Log.ignorePath.includes(url)) return;
+      if (Log.isColorless) {
+        message = `${timestamp} ${method}: ${url} ${status} ${endTime}ms`;
+      } else {
+        message = `${Log.styles.bold}${timestamp} ${method}: ${url} ${status} ${endTime}ms${Log.styles.reset}`;
+      }
+
+      if (Log.ignore.includes(url)) return;
 
       console.log(message);
     });
@@ -58,4 +65,3 @@ class Log {
 }
 
 export const logExpress = Log.logCalls;
-export const logIgnorePath = Log.logIgnorePath;
